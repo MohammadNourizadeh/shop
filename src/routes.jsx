36e5551fbce1514063bbe27a2d57205072ca9e75
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-03 21:12:33 +0330

    path of pages are ready

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,15 +2,19 @@ import { Navigate } from "react-router-dom";
 import Admin from "./layOuts/admin/Admin";
 import HomePage from "./pages/HomePage/HomePage";
 
-// export const routes = [
-//   { path: "/", element: <Navigate to={"/admin/home"} /> },
-//   {
-//     path: "/admin",
-//     element: <Admin />,
-//     children: [{ path: "/admin/home", element: <HomePage /> }],
-//   },
-// ];
-
 export const routes = [
-  
-]
+  {
+    path: "/",
+    element: <Navigate to={"/admin/home"} />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+    children: [
+      {
+        path: "/admin/home",
+        element: <HomePage />,
+      },
+    ],
+  },
+];
