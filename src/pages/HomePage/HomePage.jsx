3c3f18adca6295f78097305d6754be6736bd6595
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-04 16:34:45 +0330

    folder of card compo changed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router-dom";
-import Card from "./components/card/Card";
+import Card from "../../components/card/Card";
 import styles from "./HomePage.module.scss";
 
 export const fetchData = async () => {
