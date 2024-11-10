7e3d10ce0bdac6505ee58fcb9b9c1d30744c9fc7
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-10 20:37:05 +0330

    now you can see the count of choosed product

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,19 @@
+import { useContext } from "react";
 import ImageSwiper from "../swiper/ImageSwiper";
 import styles from "./Card.module.scss";
 import CardBtn from "./components/cardBtn/CardBtn";
 import CardInfo from "./components/cardInfo/CardInfo";
+import MainContext from "../../contexts/MainContext";
 
 export default function Card({ product, productInfoPage = false }) {
+  // context
+  const { choosedProducts } = useContext(MainContext);
+
+  // var
+  const productIndex = choosedProducts.findIndex(
+    (item) => item.id === product.id
+  );
+
   return (
     <div className={productInfoPage ? styles.infoPageStyle : styles.king}>
       <div className={styles.imgContainer}>
@@ -13,6 +23,11 @@ export default function Card({ product, productInfoPage = false }) {
           isLink={productInfoPage}
           linkAddress={`/admin/product/${product.id}`}
         />
+        {productIndex !== -1 && (
+          <div className={styles.productCountContainer}>
+            <span>{choosedProducts[productIndex].count}</span>
+          </div>
+        )}
       </div>
       <div className={styles.productInfoCont}>
         <CardInfo
