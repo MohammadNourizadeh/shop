53233301b4aa4905221828d27e018003c141ec62
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-11 17:50:51 +0800

    working on nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,10 @@
+import {
+  faBasketShopping,
+  faExclamationCircle,
+  faHome,
+  faStore,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import styles from "./Nav.module.scss";
 
@@ -6,13 +13,28 @@ export default function Nav() {
     <nav className={styles.king}>
       <ul>
         <li>
-          <Link to={"/"}>store</Link>
+          <Link to={"/"}>
+            <FontAwesomeIcon icon={faStore} />
+            store
+          </Link>
         </li>
         <li>
-          <Link to={"/"}>home</Link>
+          <Link to={"/"}>
+            <FontAwesomeIcon icon={faHome} />
+            home
+          </Link>
         </li>
         <li>
-          <Link to={"/"}>about</Link>
+          <Link to={"/"}>
+            <FontAwesomeIcon icon={faExclamationCircle} />
+            about
+          </Link>
+        </li>
+        <li className={styles.basketItemContainer}>
+          <Link to={"/"} className={styles.basketItemLink}>
+            <span className={styles.basketItemsCount}>2</span>
+            <FontAwesomeIcon icon={faBasketShopping} />
+          </Link>
         </li>
       </ul>
     </nav>
