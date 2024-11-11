9ac2c742c1eef983beaf278d64783d6e509e8dad
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-12 00:36:56 +0330

    now you can focus on navbr items

--- src/components/nav/navItemsList/NavItemsList.jsx
+++ src/components/nav/navItemsList/NavItemsList.jsx
@@ -1,11 +1,11 @@
 import {
-    faBasketShopping,
-    faExclamationCircle,
-    faHome,
-    faStore,
+  faBasketShopping,
+  faExclamationCircle,
+  faHome,
+  faStore,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import MainContext from "../../../contexts/MainContext";
 import styles from "./NavItemsList.module.scss";
@@ -14,28 +14,60 @@ export default function NavItemsList() {
   // context
   const { choosedProducts } = useContext(MainContext);
 
+  // state
+  const [focusedpage, setFocusedPage] = useState("home");
+
+  // func
+  const handleFocusedPage = (pageName) => {
+    setFocusedPage(pageName);
+  };
+
   return (
     <ul className={styles.king}>
       <li>
-        <Link to={"/"}>
+        <Link
+          className={focusedpage === "home" ? styles.focused : ""}
+          to={"/"}
+          onClick={() => {
+            handleFocusedPage("home");
+          }}
+        >
           <FontAwesomeIcon icon={faHome} />
           home
         </Link>
       </li>
       <li>
-        <Link to={"/"}>
+        <Link
+          className={focusedpage === "store" ? styles.focused : ""}
+          to={"/"}
+          onClick={() => {
+            handleFocusedPage("store");
+          }}
+        >
           <FontAwesomeIcon icon={faStore} />
           store
         </Link>
       </li>
       <li>
-        <Link to={"/"}>
+        <Link
+          className={focusedpage === "about" ? styles.focused : ""}
+          to={"/"}
+          onClick={() => {
+            handleFocusedPage("about");
+          }}
+        >
           <FontAwesomeIcon icon={faExclamationCircle} />
           about
         </Link>
       </li>
       <li className={styles.basketItemContainer}>
-        <Link to={"/"} className={styles.basketItemLink}>
+        <Link
+          to={"/"}
+          onClick={() => {
+            handleFocusedPage("basket");
+          }}
+          className={focusedpage === "basket" ? styles.focused : ""}
+        >
           {choosedProducts.length > 0 && (
             <span className={styles.basketItemsCount}>
               {choosedProducts.length}
