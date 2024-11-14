2bf2c9dd40b96689c48a0f993abd8250f46dd700
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-14 13:51:05 +0330

    basketPage card is a component now

--- src/pages/basketPage/BasketPage.jsx
+++ src/pages/basketPage/BasketPage.jsx
@@ -1,9 +1,7 @@
 import { useContext } from "react";
-import CardBtn from "../../components/card/components/cardBtn/CardBtn";
 import MainContext from "../../contexts/MainContext";
 import styles from "./BasketPage.module.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMultiply } from "@fortawesome/free-solid-svg-icons";
+import ChoosedCard from "./components/choosedCard/ChoosedCard";
 
 export default function BasketPage() {
   // context
@@ -12,24 +10,7 @@ export default function BasketPage() {
   return (
     <div className={styles.king}>
       {choosedProducts.map((choosedProduct) => (
-        <div key={choosedProduct.id} className={styles.choosedCard}>
-          <div className={styles.productImgContainer}>
-            <img src={choosedProduct.images[0]} alt={choosedProduct.title} />
-          </div>
-          <div className={styles.productInfo}>
-            <div>{choosedProduct.title}</div>
-            <div className={styles.productPrice}>$ {choosedProduct.price}</div>
-            <div>
-              <CardBtn product={choosedProduct} />
-            </div>
-          </div>
-          <div className={styles.productCountContainer}>
-            <span>
-              <FontAwesomeIcon icon={faMultiply} />
-              {choosedProduct.count}
-            </span>
-          </div>
-        </div>
+        <ChoosedCard product={choosedProduct} key={choosedProduct.id} />
       ))}
     </div>
   );
---
+++ src/pages/basketPage/components/choosedCard/ChoosedCard.jsx
@@ -0,0 +1,30 @@
+import { faMultiply } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import CardBtn from "../../../../components/card/components/cardBtn/CardBtn";
+import styles from "./ChoosedCard.module.scss";
+
+export default function ChoosedCard({ product }) {
+  return (
+    <div className={styles.king}>
+      <div className={styles.productImgContainer}>
+        <img src={product.images[0]} alt={product.title} />
+      </div>
+
+      <div className={styles.productInfo}>
+        <div>{product.title}</div>
+        <div className={styles.productPrice}>$ {product.price}</div>
+        <div>
+          <CardBtn product={product} />
+        </div>
+      </div>
+
+      <div className={styles.productCountContainer}>
+        <span>
+          <FontAwesomeIcon icon={faMultiply} />
+          {product.count}
+        </span>
+      </div>
+    </div>
+  );
+}
