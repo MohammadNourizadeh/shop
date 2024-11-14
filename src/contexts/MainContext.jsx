97bb46843f3b201124282bfaef9d082d77298284
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-14 14:01:33 +0330

    add a new state for total prices

--- src/contexts/MainContext.jsx
+++ src/contexts/MainContext.jsx
@@ -4,12 +4,15 @@ const MainContext = createContext(null);
 
 export const MainContextProvider = ({ children }) => {
   const [choosedProducts, setChoosedProducts] = useState([]);
+  const [sumPrices, setSumPrices] = useState(0);
 
   return (
     <MainContext.Provider
       value={{
         choosedProducts,
         setChoosedProducts,
+        sumPrices,
+        setSumPrices,
       }}
     >
       {children}
