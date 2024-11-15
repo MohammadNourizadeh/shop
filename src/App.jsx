278e46b36a68d978513d4614cf7e7ba0472aa64a
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-15 16:47:30 +0330

    imports بخقئشفثی

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { routes } from "./routes";
 
