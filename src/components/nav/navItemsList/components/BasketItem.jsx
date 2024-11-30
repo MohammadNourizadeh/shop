958ed1aae210e72c0c4c17b47859fe6109f30774
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-11-30 19:04:02 +0330

    hover on navbar baket item ;)

--- src/components/nav/navItemsList/components/BasketItem.jsx
+++ src/components/nav/navItemsList/components/BasketItem.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import MainContext from "../../../../contexts/MainContext";
+import ChoosedCard from "../../../../pages/basketPage/components/choosedCard/ChoosedCard";
 import styles from "./BasketItem.module.scss";
 
 export default function BasketItem({ focusedPage, onFocus }) {
@@ -25,6 +26,12 @@ export default function BasketItem({ focusedPage, onFocus }) {
         )}
         <FontAwesomeIcon icon={faBasketShopping} />
       </Link>
+
+      <div className={styles.dropDown} id={styles.showDropDown}>
+        {choosedProducts.map((choosedProduct) => (
+          <ChoosedCard product={choosedProduct} key={choosedProduct.id} />
+        ))}
+      </div>
     </li>
   );
 }
